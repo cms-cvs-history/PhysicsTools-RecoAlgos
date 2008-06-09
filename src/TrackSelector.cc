9ffdb6e58baa47fdfc4d1783279a345517cd68ed
cmsbuild <>
2008-06-09 14:07:52 +0000

    This commit was manufactured by cvs2git to create tag 'PhysicsTools- RecoAlgos-V08-06-02-06'.
    Sprout from master 2008-04-21 10:47:12 UTC Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch> 'added new module'
    Cherrypick from master 2008-06-09 14:07:49 UTC Giuseppe Cerati <sha1-fba32708710cbf239b252728f58f96c6304a00dd@cern.ch> 'add option to skip quality selection':
        interface/RecoTrackSelector.h
        interface/TrackSelector.h
        plugins/BuildFile
    Cherrypick from master 2007-09-21 08:45:56 UTC Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch> 'added new merger modules':
        plugins/BasicClusterMerger.cc
    Cherrypick from master 2008-06-05 20:55:08 UTC Petar Maksimovic <sha1-f801b06403fb61272bb7dab37fff23c67b271eee@cern.ch> '':
        src/TrackSelector.cc

---
+++ src/TrackSelector.cc
@@ -0,0 +1,198 @@
+#include "PhysicsTools/RecoAlgos/interface/TrackSelector.h"
+
+#include "DataFormats/TrackerRecHit2D/interface/SiPixelRecHit.h"
+#include "DataFormats/TrackerRecHit2D/interface/SiStripRecHit2D.h"
+#include "DataFormats/TrackerRecHit2D/interface/ProjectedSiStripRecHit2D.h"
+#include "DataFormats/TrackerRecHit2D/interface/SiStripMatchedRecHit2D.h"
+
+using namespace reco;
+
+namespace helper 
+{
+
+  TrackCollectionStoreManager::
+  TrackCollectionStoreManager(const edm::Handle<reco::TrackCollection> & ) 
+    :
+    selTracks_( new reco::TrackCollection ),
+    selTrackExtras_( new reco::TrackExtraCollection ),
+    selHits_( new TrackingRecHitCollection ),
+    selStripClusters_( new edmNew::DetSetVector<SiStripCluster> ),
+    selPixelClusters_( new edmNew::DetSetVector<SiPixelCluster> ),
+    rTracks_(), rTrackExtras_(), rHits_(), rStripClusters_(), rPixelClusters_(),
+    idx_(0), hidx_(0), scidx_(), pcidx_(),
+    cloneClusters_ (true)
+  {
+  }
+  
+  
+  
+  //------------------------------------------------------------------
+  //!  Process a single track.  THIS IS WHERE ALL THE ACTION HAPPENS!
+  //------------------------------------------------------------------
+  void 
+  TrackCollectionStoreManager::
+  processTrack( const Track & trk ) 
+  {
+    selTracks_->push_back( Track( trk ) );
+    selTracks_->back().setExtra( TrackExtraRef( rTrackExtras_, idx_ ++ ) );
+    selTrackExtras_->push_back( TrackExtra( trk.outerPosition(), trk.outerMomentum(), trk.outerOk(),
+					    trk.innerPosition(), trk.innerMomentum(), trk.innerOk(),
+					    trk.outerStateCovariance(), trk.outerDetId(),
+					    trk.innerStateCovariance(), trk.innerDetId(),
+					    trk.seedDirection() ) );
+    TrackExtra & tx = selTrackExtras_->back();
+    for( trackingRecHit_iterator hit = trk.recHitsBegin(); hit != trk.recHitsEnd(); ++ hit ) {
+      selHits_->push_back( (*hit)->clone() );
+      TrackingRecHit * newHit = & (selHits_->back());
+      tx.add( TrackingRecHitRef( rHits_, hidx_ ++ ) );
+
+      //--- Skip the rest for this hit if we don't want to clone the cluster.
+      //--- The copy constructer in the rec hit will copy the link properly.
+      //    FIXME: check whether this is true.
+      //
+      if (cloneClusters() == false)
+	continue;       // go to the next hit on the track
+
+     
+
+      //--- Copy strip or pixel cluster.  This is a bit tricky since
+      //--- the TrackingRecHit could be either a SiStripRecHit of some kind
+      //--- or the SiPixelRecHit.
+      
+      const DetId detId( (*hit)->geographicalId() );
+      if (detId.det() != DetId::Tracker) {
+	// FIXME: Throw an exception, or simply return with error status.
+	// FIXME: Could a reco::Track even have non-tracker hits???
+	assert(0);
+      }
+
+      //--- Figure out which kind of hit this is.      
+      //    (Note: hit is an iterator to a vector of RecHit pointers, so
+      //     *hit is a pointer to a hit.)
+      //
+      if (detId.subdetId() == PixelSubdetector::PixelBarrel || 
+	  detId.subdetId() == PixelSubdetector::PixelEndcap ) {
+	// must be a Pixel hit
+	const SiPixelRecHit * pixHit = dynamic_cast<const SiPixelRecHit*>( &**hit );
+	if (!pixHit) {
+	  assert(0);   	  // FIXME: throw a fatal exception
+	}
+	//--- Get the pixel cluster, clone it, save a ref.  
+	//--- In edmNew::DSV, we need to use the FastFiller
+	//--- to add a copy of the cluster to a new DSV<SiPixelCluster>
+	DetId detid = pixHit->geographicalId();
+	edmNew::DetSetVector<SiPixelCluster>::FastFiller pixFF( *selPixelClusters_, detid );
+
+	// Get the cluster. (cluster() returns an edm::Ref<edm::DetSetVector<SiPixelCluster>, SiPixelCluster >  
+	const SiPixelCluster * pixCl = &* (pixHit->cluster());
+	pixFF.push_back( *pixCl );
+
+	// Create a persistent edm::Ref to the cluster
+	edm::Ref< edmNew::DetSetVector<SiPixelCluster>, SiPixelCluster > 
+	  pixClRef( rPixelClusters_ , pcidx_ ++ );
+
+	// We must cast since setClusterRef() is not in the base class
+	SiPixelRecHit * newPixHit = dynamic_cast<SiPixelRecHit*>( newHit );
+	newPixHit->setClusterRef( pixClRef );
+      } 
+      //
+      else { 
+	//
+        //--- should be a SiStrip hit (one-dimensional) 
+	//
+ 	const SiStripRecHit2D * sHit1 
+	  = dynamic_cast<const SiStripRecHit2D*>( &**hit );
+	if (sHit1) {
+	  cloneStripHit( newHit, sHit1 );
+ 	  continue;       // go to the next hit on the track
+	}
+	//
+	//--- Either matched or projected
+	//
+	const SiStripMatchedRecHit2D * sHit2 
+	  = dynamic_cast<const SiStripMatchedRecHit2D*>( &**hit ); 
+	if (sHit2) {
+	  //--- Clone stereo side
+	  const SiStripRecHit2D * stereoHit1 = sHit2->stereoHit();
+	  cloneStripHit( newHit, stereoHit1 );
+	  //--- Clone mono side
+	  const SiStripRecHit2D * monoHit1 = sHit2->monoHit();
+	  cloneStripHit( newHit, monoHit1 );
+	  continue;       // go to the next hit on the track
+	}
+	//
+	//--- Must be the projected one...
+	//
+	const ProjectedSiStripRecHit2D * sHit3 
+	  = dynamic_cast<const ProjectedSiStripRecHit2D*>( &**hit );
+	if (sHit3) {
+	  //--- Get the original hit and clone the cluster from that one
+	  const SiStripRecHit2D & oHit1 = sHit3->originalHit();
+	  cloneStripHit( newHit, &oHit1 );
+	  continue;       // go to the next hit on the track
+	}
+	//--- If we are here, we are in trouble
+	edm::LogError("UnkownType") 
+	  << "@SUB=AlignmentTrackSelector::isHit2D"
+	  << "Tracker hit not in pixel and neither SiStripRecHit2D nor "
+	  << "SiStripMatchedRecHit2D nor ProjectedSiStripRecHit2D.";
+	// &&& throw an exception?
+	return;
+
+      } // end of if (PixelBarrel || PixelEndcap) (namely pixel hit) -- else strip hit
+
+
+
+    } // end of for loop over tracking rec hits on this track
+  }
+
+
+  void
+  TrackCollectionStoreManager::
+  cloneStripHit( TrackingRecHit * newHit,          // new hit we just cloned
+		 const SiStripRecHit2D * sHit1 )   // old one (or its component), cast
+  {
+    //
+    //--- Get the strip cluster, clone it, save a ref.  
+    //--- In edmNew::DSV, we need to use the FastFiller
+    //--- to add a copy of the cluster to a new DSV<SiStripCluster>
+    DetId detid = sHit1->geographicalId();
+    edmNew::DetSetVector<SiStripCluster>::FastFiller siFF( *selStripClusters_, detid );
+    
+    // Get the cluster. (cluster() returns an edm::Ref<edm::DetSetVector<SiStripCluster>, SiStripCluster >  
+    const SiStripCluster * strCl = &* (sHit1->cluster());
+    siFF.push_back( *strCl );
+    
+    // Create a persistent edm::Ref to the cluster
+    edm::Ref<edmNew::DetSetVector<SiStripCluster>,SiStripCluster > 
+      strClRef( rStripClusters_ , scidx_ ++ );
+    
+    // We must cast since setClusterRef() is not in the base class
+    SiStripRecHit2D * newStrHit = dynamic_cast<SiStripRecHit2D*>( newHit );
+    newStrHit->setClusterRef( strClRef );
+  }
+
+
+
+
+
+  
+
+  //------------------------------------------------------------------
+  //!  Put tracks, track extras and hits+clusters into the event.
+  //------------------------------------------------------------------
+  edm::OrphanHandle<reco::TrackCollection> 
+  TrackCollectionStoreManager::
+  put( edm::Event & evt ) {
+    edm::OrphanHandle<reco::TrackCollection> 
+      h = evt.put( selTracks_ );
+    evt.put( selTrackExtras_ );
+    evt.put( selHits_ );
+    evt.put( selStripClusters_ );
+    evt.put( selPixelClusters_ );
+    return h; 
+  }
+  
+
+} // end of namespace helper
+
